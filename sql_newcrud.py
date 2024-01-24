6acc45633e7fb066120cc62174526898796e7833
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-24 13:01:53 +0000

    Create instans of mobile class add to table

--- sql_newcrud.py
+++ sql_newcrud.py
@@ -14,12 +14,44 @@ class Mobile(base):
     model = Column(String)
     release = Column(String)
     price = Column(Float)
-
+ 
 Session = sessionmaker(db)
 session = Session()
 
-base.metadata.create_all(db)
+base.metadata.create_all(db) 
+"""
+base.metadata.drop_all(db)
+"""
+Samsung = Mobile(
+    brand = "samsung",
+    model = "fold 5",
+    release = "August 23",
+    price = 1600.00
+    )
+Samsungul = Mobile(
+    brand = "samsung",
+    model = "s24 Ultra",
+    release = "january 24",
+    price = 1240.00
+    )
+oneplus = Mobile(
+    brand = "Oneplus",
+    model = "11 pro",
+    release = "january 23",
+    price = 840.00
+    )
+session.add(Samsung)
+session.add(Samsungul)
+session.add(oneplus)
+session.commit()
 
-
+try:
+    moiles = session.query(Mobile)
+    for m in moiles:
+        print(m.model,m.price,sep="||")
+  
+            
+except Exception as e:
+    print(e)
 
 
